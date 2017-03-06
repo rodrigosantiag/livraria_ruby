e596b814911dce5f71f7cece04724bf45f612688
Rodrigo Santiago <rodrigo@santive.com>
2017-03-06 19:31:10 -0300

    Cria o atributo possui reimpressão e um método que o retorna

--- livro.rb
+++ livro.rb
@@ -1,10 +1,23 @@
 class Livro
-  attr_reader :titulo, :preco, :ano_lancamento
+  attr_reader :titulo, :preco, :ano_lancamento, :possui_reimpressao
 
-  def initialize(titulo, preco, ano_lancamento)
+  def initialize(titulo, preco, ano_lancamento, possui_reimpressao)
     @titulo = titulo
     @ano_lancamento = ano_lancamento
     @preco = aplica_promocao preco
+    @possui_reimpressao = possui_reimpressao
+  end
+
+  def possui_reimpressao?
+    @possui_reimpressao
+  end
+
+  def exibe_newsletter(livro)
+    if livro.ano_lancamento < 2000
+      puts "Newsletter"
+      puts livro.titulo
+      puts livro.preco
+    end
   end
 
   private
@@ -17,10 +30,11 @@ class Livro
   end
 end
 
-def exibe_newsletter(livro)
-  if livro.ano_lancamento < 2000
-    puts "Newsletter"
-    puts livro.titulo
-    puts livro.preco
-  end
+agile = Livro.new("Agile Development with Rails", 100, 1998, true)
+newest_agile = Livro.new("Agile Development with Rails (Second Edition)", 100, 2012, true)
+
+livros = [agile, newest_agile]
+
+livros.each do |livro|
+  livro.exibe_newsletter livro
 end
