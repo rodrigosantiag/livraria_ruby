7d3c0a8c250398d76a23a25a6d81e27f78dbc8a9
Rodrigo Santiago <rodrigo@santive.com>
2017-03-06 19:58:12 -0300

    Refatoração do método calcula_preco Criação da classe estoque Criação dos métodos "initialize", "total" e "mais_baratos_que" na classe "Estoque"

--- livro.rb
+++ livro.rb
@@ -4,7 +4,7 @@ class Livro
   def initialize(titulo, preco, ano_lancamento, possui_reimpressao)
     @titulo = titulo
     @ano_lancamento = ano_lancamento
-    @preco = aplica_promocao preco
+    @preco = calcula_preco preco
     @possui_reimpressao = possui_reimpressao
   end
 
@@ -21,20 +21,57 @@ class Livro
   end
 
   private
-  def aplica_promocao(base)
-    if @ano_lancamento < 2000
-      base *= 0.7
+  def calcula_preco(base)
+    if @ano_lancamento < 2006
+      if !possui_reimpressao?
+        base * 0.95
+      else
+        base * 0.9
+      end
+    elsif @ano_lancamento <= 2010
+      base * 0.96
     else
       base
     end
   end
 end
 
-agile = Livro.new("Agile Development with Rails", 100, 1998, true)
-newest_agile = Livro.new("Agile Development with Rails (Second Edition)", 100, 2012, true)
+class Estoque
+  attr_reader :livros
 
-livros = [agile, newest_agile]
+  def initialize
+    @livros = []
+  end
 
-livros.each do |livro|
-  livro.exibe_newsletter livro
+  def exporta_csv
+    @livros.each do |livro|
+      puts "#{livro.titulo},#{livro.ano_lancamento}"
+    end
+  end
+
+  def total
+    @livros.size
+  end
+
+  def mais_baratos_que(valor)
+    @livros.select do |livro|
+      livro.preco < valor
+    end
+  end
+
 end
+
+algoritmos = Livro.new("Algoritmos", 100, 1998, true)
+arquitetura = Livro.new("Introdução à Arquitetura e Design de Software", 70, 2011, true)
+
+#estoque = [algoritmos, arquitetura]
+
+estoque = Estoque.new
+
+estoque.livros << algoritmos
+estoque.livros << arquitetura
+
+baratos = estoque.mais_baratos_que 800
+
+puts baratos
+puts estoque.total
