8384d14ab99a2ba2036dec7aad1602c2912bbc6d
Rodrigo Santiago <rodrigo@santive.com>
2017-03-20 21:34:36 -0300

    Acrescentado o atributo "tipo" ao livro. Assim filtramos os produto mais vendidos por tipo (livros, revistas, ebooks, etc.)

--- estoque.rb
+++ estoque.rb
@@ -48,8 +48,8 @@ class Estoque
     @vendas.count{|venda| campo.call(venda) == campo.call(produto) }
   end
 
-  def livro_que_mais_vendeu_por(&campo)
-    @vendas.sort {|v1,v2| quantidade_de_vendas_por(v1, &campo) <=> quantidade_de_vendas_por(v2, &campo)}.last
+  def que_mais_vendeu_por(tipo, &campo)
+    @vendas.select{|t| t.tipo == tipo}.sort {|v1,v2| quantidade_de_vendas_por(v1, &campo) <=> quantidade_de_vendas_por(v2, &campo)}.last
   end
 
 end
--- sistema.rb
+++ sistema.rb
@@ -1,18 +1,27 @@
 require_relative "livro"
 require_relative "estoque"
 
-algoritmos = Livro.new("Algoritmos ** teste", 100, 1998, true, "Editora 1")
-arquitetura = Livro.new("Introdução à Arquitetura e Design de Software", 70, 2011, true, "Editora 2")
-programmer = Livro.new("Livro Programmer", 150, 1999, true, "Editora 3")
-outro_livro = Livro.new("Livro Qualuer", 200, 2017, true, "Editora 1")
+algoritmos = Livro.new("Algoritmos ** teste", 100, 1998, true, "Editora 1", "livro")
+arquitetura = Livro.new("Introdução à Arquitetura e Design de Software", 70, 2011, true, "Editora 2", "livro")
+programmer = Livro.new("Livro Programmer", 150, 1999, true, "Editora 3", "livro")
+outro_livro = Livro.new("Livro Qualuer", 200, 2017, true, "Editora 1", "livro")
+revistona = Livro.new("Revista Ruby", 20, 2012, true, "Editora de revista", "revista")
+revistona2 = Livro.new("Revista Ruby Refatoração", 10, 1998, true, "Editora de revista", "revista")
 
 estoque = Estoque.new
 
-estoque << algoritmos << algoritmos << algoritmos << arquitetura << programmer << outro_livro << outro_livro
+estoque << algoritmos << algoritmos << algoritmos << arquitetura << programmer << outro_livro << outro_livro << revistona << revistona << revistona2
 estoque.vende algoritmos
 estoque.vende algoritmos
 estoque.vende programmer
 estoque.vende programmer
 estoque.vende programmer
 
-puts estoque.livro_que_mais_vendeu_por(&:editora).titulo
+estoque.vende revistona
+estoque.vende revistona
+estoque.vende revistona2
+estoque.vende revistona2
+estoque.vende revistona2
+
+puts estoque.que_mais_vendeu_por("livro", &:titulo).titulo
+puts estoque.que_mais_vendeu_por("revista", &:titulo).titulo
